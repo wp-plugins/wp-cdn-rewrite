31ec85c1a4690a6c28caf36d8c8574b3892b1db0
csloisel <csloisel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-22 16:13:38 +0000

    tagging version: 0.1.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-cdn-rewrite/tags/0.1.3@952916 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ vendor/voceconnect/voce-settings-api/display-callbacks.php
@@ -0,0 +1,48 @@
+<?php
+
+function vs_display_text_field($value, $setting, $args){
+	?>
+	<input name="<?php echo esc_attr( $setting->get_field_name() ); ?>" id="<?php echo esc_attr( $setting->get_field_id() ) ?>" value="<?php echo esc_attr($value) ?>" class="regular-text" type="text">
+	<?php if(!empty($args['description'])) : ?>
+		<br/><span class="description"><?php echo wp_kses_post( $args['description'] ); ?></span>
+	<?php endif;
+}
+
+function vs_display_dropdown($value, $setting, $args) {
+	if(!isset($args['options'])) {
+		?>
+		<p class="error">An options argument is required in the $args array to use vs_display_dropdown()</p>
+		<?php
+		return;
+	} else {
+		?>
+		<select id="<?php echo esc_attr( $setting->get_field_id() ); ?>" name="<?php echo esc_attr( $setting->get_field_name() ) ?>">
+			<?php
+			foreach( $args['options'] as $option_value => $option_text ) {
+				$selected = ( $option_value == $value ) ? 'selected="selected"' : '';
+				echo sprintf( "<option value='%s' %s</option>", esc_attr( $option_value ), selected( $option_value, $value, false ) );
+			}
+			?>
+		</select>
+		<?php if(!empty($args['description'])) : ?>
+			<br/><span class="description"><?php echo wp_kses_post( $args['description'] ); ?></span>
+		<?php endif;
+	}
+}
+
+function vs_display_textarea($value, $setting, $args) {
+	?>
+	<textarea id="<?php echo esc_attr( $setting->get_field_id() ); ?>" name="<?php echo esc_attr( $setting->get_field_name() ) ?>" rows='7' cols='50' type='textarea'><?php echo esc_html($value) ?></textarea>
+	<?php if(!empty($args['description'])) : ?>
+		<br/><span class="description"><?php echo wp_kses_post( $args['description'] ); ?></span>
+	<?php endif; ?>
+	<?php
+}
+
+function vs_display_checkbox($value, $setting, $args) {
+	?>
+	<input type="checkbox" id="<?php echo esc_attr( $setting->get_field_id() ); ?>" name="<?php echo esc_attr( $setting->get_field_name() ) ?>" <?php checked( $value, 'on' ) ?> />
+	<?php if(!empty($args['description'])) : ?>
+		<br/><span class="description"><?php echo wp_kses_post( $args['description'] ); ?></span>
+	<?php endif;
+}
